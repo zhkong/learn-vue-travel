e8c0fc2dffac0c14e04d61b94f5a1e0ef2a7b7a5
zhkong <klarkzh@hotmail.com>
2021-04-02 09:48:39 +0800

    add home swiper

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <span class="iconfont">&#xe685;</span>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe67d; </span>
+      输入城市/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont">&#xe688;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style>
+/* scoped ?*/
+html {
+  --main-color: #06b3d2;
+}
+
+.header {
+  color: #fff;
+  line-height: 0.86rem;
+  background-color: var(--main-color);
+  display: flex;
+}
+.header .header-input {
+  margin: auto 0.1rem;
+  background: #fff;
+  color: #ccc;
+  flex: 1;
+  border-radius: 0.1rem;
+  height: 0.64rem;
+  line-height: 0.6rem;
+  align-items: center;
+  padding-left: 0.2rem;
+}
+.header .header-left {
+  width: 0.64rem;
+  float: left;
+  display: block;
+  text-align: center;
+}
+.header .header-right {
+  width: 1.24rem;
+  float: right;
+  text-align: center;
+}
+</style>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="swiper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id"
+        ><img class="image-swiper" :src="item.imageUrl" />\
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        autoplay: 3000,
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '001',
+          imageUrl: 'https://source.qunarzz.com/site/images/wns/20210316_qunar_dujia_homepage_750x192_2.jpg'
+        },
+        {
+          id: '002',
+          imageUrl: 'https://source.qunarzz.com/site/images/wns/20210319_qunar_dujia_homepage_750_192_3.jpg'
+        },
+        {
+          id: '003',
+          imageUrl: 'https://source.qunarzz.com/site/images/wns/20210313_qunar_dujia_750x192_6.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.swiper {
+  width: 100%;
+  /* height:25.6vw; 防止图片没有加载成功后面内容抖动。删除以下内容他用这个也可以*/
+  height: 0;
+  overflow: hidden;
+  padding-bottom: 25.6%;
+  background: #eee;
+}
+
+.image-swiper {
+  width: 100%;
+}
+</style>
