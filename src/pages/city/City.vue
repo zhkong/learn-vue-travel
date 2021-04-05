097b1166ab70d52341cc7676c2ae754499fc3ccd
zhkong <klarkzh@hotmail.com>
2021-04-05 16:55:58 +0800

    add city-search

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,14 +1,19 @@
 <template>
-  <CityHeader />
+  <div>
+    <CityHeader />
+    <CitySearch />
+  </div>
 </template>
 
 <script>
 import CityHeader from './components/Header'
+import CitySearch from './components/Search'
 
 export default {
   name: 'City',
   components: {
-    CityHeader
+    CityHeader,
+    CitySearch
   }
 }
 </script>
---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="city-search">
+    <input type="text" class="city-input" placeholder="请输入城市名称" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style scoped>
+.city-search {
+  background: #06b3d2;
+  height: 0.6rem;
+  padding:0 .12rem;
+}
+.city-search .city-input {
+  width: 100%;
+  height: 0.5rem;
+  line-height: 0.5rem;
+  text-align: center;
+  border-radius: 0.1rem;
+  color:#666;
+}
+</style>
