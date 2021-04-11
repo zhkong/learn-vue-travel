83248101d3a3ef04e50a3dd664589be01efb9acb
zhkong <klarkzh@hotmail.com>
2021-04-11 19:58:03 +0800

    删除fastclick使用css

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import fastClick from 'fastclick'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 import store from './store'
 import '@/assets/styles/reset.css'
@@ -13,7 +12,6 @@ import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 
-fastClick.attach(document.body)
 Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
