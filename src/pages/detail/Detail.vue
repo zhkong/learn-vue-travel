207f5699c2102f79611f8ebdcb54e09265a729ff
zhkong <klarkzh@hotmail.com>
2021-04-15 21:39:14 +0800

    拆分detail组件

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,31 +1,23 @@
 <template>
   <div>
-    <div class="gallery-header" @click="handleImageShown">
-      <div class="header-image">
-        <img :src="imageList[0]" />
-      </div>
-      <div class="gallery-header-text">
-        <span class="text">大连圣亚海洋世界(AAAA景区) </span>
-        <span class="image-count iconfont"
-          >&#xe612; {{ imageList.length }}</span
-        >
-      </div>
-    </div>
+    <detail-banner :imageList="imageList" @shown="handleImageShown" />
     <gallery
       v-show="isShown"
       :isShown="isShown"
       :imageList="imageList"
       @close="handleImageShown"
-    ></gallery>
+    />
   </div>
 </template>
 
 <script>
 import Gallery from 'common/gallery/Gallery'
+import DetailBanner from './components/Banner'
 export default {
   name: 'Detail',
   components: {
-    Gallery
+    Gallery,
+    DetailBanner
   },
   data() {
     return {
@@ -45,39 +37,4 @@ export default {
 }
 </script>
 
-<style scoped>
-.gallery-header {
-  width: 100%;
-  height: 55vw;
-  position: relative;
-  color: #fff;
-}
-
-.gallery-header img {
-  width: 100%;
-}
-
-.gallery-header-text {
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  display: flex;
-  padding: 0.15rem 0;
-  background: linear-gradient(top, transparent, rgba(0, 0, 0, 0.9));
-}
-
-.gallery-header-text .text {
-  flex: 1;
-  padding-left: 0.2rem;
-}
-.gallery-header-text .image-count {
-  padding: 0.05rem 0.2rem;
-  background: rgba(23, 23, 23, 0.9);
-  border-radius: 0.15rem 0 0 0.15rem;
-}
-
-.image-count {
-  font-size: 0.3rem;
-}
-</style>
+<style scoped></style>
---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="gallery-banner" @click="handleImageShown">
+    <div class="banner-image">
+      <img :src="imageList[0]" />
+    </div>
+    <div class="gallery-banner-text">
+      <span class="text">大连圣亚海洋世界(AAAA景区) </span>
+      <span class="image-count iconfont">&#xe612; {{ imageList.length }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Banner',
+  props: {
+    imageList: Array
+  },
+  methods: {
+    handleImageShown() {
+      this.$emit('shown')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.gallery-banner {
+  width: 100%;
+  height: 55vw;
+  position: relative;
+  color: #fff;
+}
+
+.gallery-banner img {
+  width: 100%;
+}
+
+.gallery-banner-text {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  padding: 0.15rem 0;
+  background: linear-gradient(top, transparent, rgba(0, 0, 0, 0.9));
+}
+
+.gallery-banner-text .text {
+  flex: 1;
+  padding-left: 0.2rem;
+}
+.gallery-banner-text .image-count {
+  padding: 0.05rem 0.2rem;
+  background: rgba(23, 23, 23, 0.9);
+  border-radius: 0.15rem 0 0 0.15rem;
+}
+
+.image-count {
+  font-size: 0.3rem;
+}
+</style>
